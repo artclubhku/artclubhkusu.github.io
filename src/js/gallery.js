a957ca45f7b773c808346d7c82f864e07af40677
artclubhkusu <artclubhkusu@gmail.com>
2021-08-05 22:34:49 +0800

    fixed gallery, really really for real this time

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,4 @@
 var gallery = document.querySelector('#gallery');
-
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
 var resizeAll = function () {
@@ -10,20 +9,24 @@ var resizeAll = function () {
         el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
     });
 };
-
 gallery.querySelectorAll('img').forEach(function (item) {
     item.classList.add('byebye');
-    // item.addEventListener('load', function () {
-    //     var altura = getVal(gallery, 'grid-auto-rows');
-    //     var gap = getVal(gallery, 'grid-row-gap');
-    //     var gitem = item.parentElement.parentElement;
-    //     gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-    //     item.classList.remove('byebye');
-    // });
-    var altura = getVal(gallery, 'grid-auto-rows');
-    var gap = getVal(gallery, 'grid-row-gap');
-    var gitem = item.parentElement.parentElement;
-    gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+    if (item.complete) {
+        var altura = getVal(gallery, 'grid-auto-rows');
+        var gap = getVal(gallery, 'grid-row-gap');
+        var gitem = item.parentElement.parentElement;
+        gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+        item.classList.remove('byebye');
+    }
+    else {
+        item.addEventListener('load', function () {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            var gitem = item.parentElement.parentElement;
+            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+            item.classList.remove('byebye');
+        });
+    }
 });
 
 window.addEventListener('resize', resizeAll);
